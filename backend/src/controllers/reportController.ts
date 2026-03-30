448abee0e98b99221bcdd205799defda5273ddf7
AnmolDholiya <anmoldholiya026@gmail.com>
2026-03-30 22:33:37 +0530

    Initial commit

--- backend/src/controllers/reportController.ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response } from 'express';
-
-export async function createReport(_req: Request, res: Response) {
-  res.status(501).json({ message: 'Not implemented: createReport' });
-}
-
-
